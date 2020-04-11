b665e2f489f6b848898a3596e9c555b90d512e4d
gunneone <gunneone@gmail.com>
2020-04-11 10:57:20 -0400

    Added gitignore. Fixed combination calculation. Added logging to file. Generally beautified.

--- src/config.py
+++ src/config.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 games = 1
 survivorGames = 20
 verbose = False
@@ -8,3 +9,15 @@ displayThreshhold = 0.2
 killThreshhold = 0.5
 phases = 3
 singleTeam = False
+=======
+games = 10
+survivorGames = 20
+verbose = False
+digits = 1
+amateur = 65
+teamAmount = 5000
+displayThreshhold = 0.7
+killThreshhold = 0.6
+phases = 1
+singleTeam = True
+>>>>>>> 092dae418aa635d75e773ac73b40bf77ea9f9ae6
