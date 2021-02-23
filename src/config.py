8acda860ce108765e0d8e53779cef38f930469f0
Gunneone <20754683+Gunneone@users.noreply.github.com>
2021-02-23 12:17:10 +0100

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,12 +1,24 @@
+# Number of played Games per combination
 games = 5
+# Not currently used
 survivorGames = 20
+# Verbose mode for debugging purposes.
 verbose = False
+# Number of digits after the comma for %-Values
 digits = 1
+# WP of the randomly generated Teams
 amateur = 93
+# How many random teams will be added to the user defined teams
 teamAmount = 500
+# Teams with results less than this will not be shown
 displayThreshhold = 0.5
+# Teams with results less than this will not advance to the next iteration
 killThreshhold = 0.5
+# Number of max iterations
 phases = 100
+# If True, only a single Team (with name esc) will be matched against all other Teams
 singleTeam = False
+# How many games will be played per combination only if singleTeam is True
 singleTeamGames = 100
+# Use points (3 for win, 1 for tie) instead of ko-system with winning percentage
 pointSystem = True
