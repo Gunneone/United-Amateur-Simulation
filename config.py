c506a9dbba5317f6396a34f6851855c494086505
Gunneone <20754683+Gunneone@users.noreply.github.com>
2021-02-23 12:09:29 +0100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+games = 5
+survivorGames = 20
+verbose = False
+digits = 1
+amateur = 93
+teamAmount = 500
+displayThreshhold = 0.5
+killThreshhold = 0.5
+phases = 100
+singleTeam = False
+singleTeamGames = 100
+pointSystem = True
